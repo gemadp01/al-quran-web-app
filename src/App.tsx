ca8db9f9d0992ec8a088a63a1ba34ebf1a18291f
Gema Dodi Pranata <gemadp01@gmail.com>
2025-07-15 21:22:29 +0700

    feat: add DetailSurahPage to App routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import DetailSurahPage from "./pages/DetailSurahPage";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/surah/:surahId" element={<DetailSurahPage />} />
+      </Routes>
     </>
   );
 }
